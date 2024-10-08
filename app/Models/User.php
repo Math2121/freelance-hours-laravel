4001a6c45008720d98f02e39ab7ed6028ed4b1e4
Matheus de Paula <matheusdepaula@wisecut.video>
2024-10-08 09:34:24 -0300

    feat: create model users and project

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,7 +19,8 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
-        'password',
+        'avatar',
+        'rating',
     ];
 
     /**
@@ -27,21 +28,9 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+    // protected $hidden = [
+    //     'password',
+    //     'remember_token',
+    // ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
-    {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
-    }
 }
